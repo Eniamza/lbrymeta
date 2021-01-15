ee07c904b1720311efab29d1e203199ea77b1ec1
Eniamsa <eniamza0@gmail.com>
2021-01-15 15:58:39 +0600

    Feat : Added more API Checks for finding real problems

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ let channelClaim = prompt(`Enter Channel Claimid:`);
 let bye ;
 let url = `https://chainquery.lbry.com/api/sql?query=SELECT%20*%20FROM%20claim%20WHERE%20publisher_id%20=%20%27${channelClaim}%27%20AND%20bid_state%20%3C%3E%20%27Spent%27`;
 
-console.log(url);
+console.log(`Fetching from ${url}`);
 axios.get(url)
 .then (response =>{
     metadata= response.data;
     metadata=JSON.stringify(metadata, null, 1);
     lbryData= JSON.parse(metadata); 
 
+    if(lbryData.success==true) {
+
         console.log(`Full Dump Mode selected`);
         console.log(`API is Working and successfully fetched. Writing Data to File!`);
         fs.writeFileSync('meta.json', metadata);
         bye = prompt(`Data is Saved in meta.json .Bela Ciao! [Press Enter to exit]`);
+    }
+    else if (lbryData.success==false){
         
         console.log("Uh oh! API is up but isn't working properly. Try again in a bit");
         bye = prompt(`\nBela Ciao! [Press Enter to exit]`);
@@ -27,7 +31,8 @@ axios.get(url)
  
 })
     .catch(error => {
-    console.log(error);
+        console.log('API seems to be down. Booo! Try again in a bit or contact in LBRY in Discord\nhttps://chat.lbry.com');
+        bye = prompt(`\nBela Ciao! [Press Enter to exit]`);
   });
 
 
