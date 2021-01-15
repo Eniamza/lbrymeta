86c5e099a9d25ad943bf77758d902f566c4a998e
Eniamsa <eniamza0@gmail.com>
2021-01-15 15:58:03 +0600

    Feat : Add Bye bye message

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const prompt = require('prompt-sync')();
 let metadata;
 let lbryData;
 let channelClaim = prompt(`Enter Channel Claimid:`);
+let bye ;
 let url = `https://chainquery.lbry.com/api/sql?query=SELECT%20*%20FROM%20claim%20WHERE%20publisher_id%20=%20%27${channelClaim}%27%20AND%20bid_state%20%3C%3E%20%27Spent%27`;
 
 console.log(url);
@@ -17,7 +18,12 @@ axios.get(url)
         console.log(`Full Dump Mode selected`);
         console.log(`API is Working and successfully fetched. Writing Data to File!`);
         fs.writeFileSync('meta.json', metadata);
-    
+        bye = prompt(`Data is Saved in meta.json .Bela Ciao! [Press Enter to exit]`);
+        
+        console.log("Uh oh! API is up but isn't working properly. Try again in a bit");
+        bye = prompt(`\nBela Ciao! [Press Enter to exit]`);
+    }
+
  
 })
     .catch(error => {
